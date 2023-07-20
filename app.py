5cae6dffce94fdd5f133c9eb1019c67b3ccd14de
Ebanksc27 <126507556+Ebanksc27@users.noreply.github.com>
2023-07-20 15:20:26 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,54 @@
-from flask import Flask
+# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
-app.route('/welcome')
-def welcome():
-    return 'welcome'
+# @app.route('/add')
+# def addition():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = add(a, b)
+#     return str(result)
 
-app.route('/welcome/home')
-def welcome_home():
-    return 'welcome home'
+# @app.route('/sub')
+# def subtraction():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = sub(a, b)
+#     return str(result)
 
-app.route('/welcome/back')
-def welcome_back():
-    return 'welcome back'
+# @app.route('/mult')
+# def multiplication():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = mult(a, b)
+#     return str(result)
+
+# @app.route('/div')
+# def division():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = div(a, b)
+#     return str(result)
+
+operations = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div
+}
+
+@app.route('/math/<operation>')
+def calculate(operation):
+    if operation not in operations:
+        return 'Invalid operation'
+
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+
+    result = operations[operation](a, b)
+    return str(result)
 
 if __name__ == '__main__':
     app.run()
